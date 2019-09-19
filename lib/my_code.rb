0dfe3a6cd657d118856f73ac48b3f9b7013c3c57
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-19 15:11:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,15 +38,14 @@ end
 return new_again_and_again
 end
 
-def reduce_to_total(source_array, starting_point)
-  
-starting_point = source_array[0]
-i = 0
-while i < source_array.length do
-  
-  
-end
-  i+=1
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0
+  while i < source_array.length do 
+    total+= source_array[i]
+    i+=1 
+  end
+  total
 end
 
 def reduce_to_all_true(source_array)
