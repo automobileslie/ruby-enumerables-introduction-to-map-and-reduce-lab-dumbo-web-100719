7a22d6903132a68c396367e0adf0b0716642260a
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-19 14:20:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,59 @@
-# My Code here....
+def map_to_negativize(source_array)
+ new = []
+  i = 0
+  while i < source_array.length do
+    new.push(source_array[i] * -1) 
+    i += 1
+  end
+  return new
+end
+
+def map_to_no_change(source_array)
+  new_again = []
+  i = 0
+  while i < source_array.length do
+    new_again.push(source_array[i])
+    i+=1
+  end
+  return new_again
+end
+
+def map_to_double(source_array)
+new_once_again = []
+i = 0
+while i < source_array.length do
+  new_once_again.push(source_array[i]*2)
+  i+=1
+end
+return new_once_again
+end
+
+def map_to_square(source_array)
+new_again_and_again = []
+i = 0
+while i < source_array.length do
+  new_again_and_again.push(source_array[i]**2)
+  i+=1
+end
+return new_again_and_again
+end
+
+def reduce_to_total(source_array, starting_point)
+  totaled_array = []
+starting_point = source_array[0]
+i = 0
+while i < source_array.length do
+  totaled_array.push(source_array[0] + source_array[i])
+  i+=1
+end
+return totaled_array
+  
+end
+
+def reduce_to_all_true(source_array)
+
+end
+
+def reduce_to_any_true(source_array)
+
+end
